140736589705e3c73876fd637980cfc80cc269fd
Edward Capriolo <edlinuxguru@gmail.com>
2012-05-13 21:20:21 -0400

    Licence stuff

--- src/test/java/com/jointhegrid/udf/collect/CollectTest.java
+++ src/test/java/com/jointhegrid/udf/collect/CollectTest.java
@@ -1,7 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+Copyright 2012 jointhegrid.com Edward Capriolo
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package com.jointhegrid.udf.collect;
 
 import com.jointhegrid.hive_test.HiveTestService;
@@ -14,10 +25,6 @@ import java.util.List;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.Path;
 
-/**
- *
- * @author edward
- */
 public class CollectTest extends HiveTestService {
 
   public CollectTest() throws IOException {
